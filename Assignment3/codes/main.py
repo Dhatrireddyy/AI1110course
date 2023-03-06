e8d8853004da7d54ed5a3839544d358911bbd6d8
Dhatrireddyy <102531062+Dhatrireddyy@users.noreply.github.com>
2023-03-06 16:31:17 +0530

    Update main.py

--- Assignment3/codes/main.py
+++ Assignment3/codes/main.py
@@ -1,31 +1,56 @@
-# ICSE 2019 Grade 12
-# Question 10
+# CBSE Probability Grade 10
+# Exercise 15.1 Q13
 
 # Name: Velma Dhatri Reddy
 # Roll number: AI21BTECH11030
 
 """ Problem Statement
-Bag A contains 4 white balls and 3 black balls, while Bag B contains 3 white balls and 5 black balls.
-Two balls are drawn from Bag A and placed in Bag B. Then, what is the probability of drawing a white
-ball from Bag B?
+A die is thrown once. Find the probability of getting
+(i) a prime number
+(ii) a number lying between 2 and 6
+(iii) an odd number
 """
+#Create a PMF and CDF subplot for a single die roll
 
-from math import comb
+def line_gen(A,B):
+    len =10
+    dim = A.shape[0]
+    x_AB = np.zeros((dim,len))
+    lam_1 = np.linspace(0,1,len)
+    for i in range(len):
+        temp1 = A + lam_1[i]*(B-A)
+        x_AB[:,i]= temp1.T
+    return x_AB
 
-def main():
-    Pr_1 = 2/7  # Probability that balls drawn from bag A are white
-    Pr_2 = 1/7  # Probability that balls drawn from bag A are black
-    Pr_3 = 4/7  # Probability that the balls drawn are black and white from bag A
-    Pr_4 = 5/10 # Probability that the ball drawn from bag B is white given both balls drawn are white
-    Pr_5= 3/10  # Probability that the ball drawn from bag B is white given both balls drawn are black
-    Pr_6 = 4/10 # Probability that the ball drawn from bag B is white given balls drawn are white and black
+#Function for batch usage of line_gen
+def batch_plot(A, B):
+    len = A.shape[0]
+    for i in range(len):
+        x_AB = line_gen(A[i, :], B[i, :])
+        plt.plot(x_AB[0, :], x_AB[1, :], 'k-')
 
-    #Output
-    print(f"The probability that the ball drawn is white from bag B is {prob(Pr_1,Pr_2,Pr_3,Pr_4,Pr_5,Pr_6)}")
-     
-def prob(Pr_1,Pr_2,Pr_3,Pr_4,Pr_5,Pr_6) -> float:
-        """ Returns the probability that the ball drawn is white from bag B """
-        return Pr_1*Pr_4 + Pr_2*Pr_5 + Pr_3*Pr_6
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.pyplot import stem
 
-if __name__ == '__main__':
-       main()
+#Arrays for PMF
+X = np.array([1, 2, 3, 4, 5, 6])
+Y = np.array([1/6, 1/6, 1/6, 1/6, 1/6, 1/6])
+Z = np.cumsum(Y)
+
+#Plotting the PMF
+plt.subplot(1, 2, 1)
+plt.xlabel('Value of X')
+plt.ylabel('Probability Mass Function')
+plt.xticks(X)
+stem(X, Y, linefmt='k-', markerfmt='ko', basefmt='k-')
+
+#Plotting the CDF
+plt.subplot(1, 2, 2)
+stem(X, Z, linefmt='k-', markerfmt='ko', basefmt='k-')
+plt.xlabel('Value of X')
+plt.ylabel('Cumulative Probability')
+plt.xticks(X)
+plt.grid()
+plt.tight_layout() #Space the subplots properly
+plt.savefig('/Users/dhatrireddy/Desktop/3_1')
